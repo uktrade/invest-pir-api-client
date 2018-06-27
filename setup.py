1133bf0571e2738758b8fd2976487ba3b3a62645
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-06-27 10:45:29 +0100

    fix requirements

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 """
-Export Directory API client
+Invest PIR API client
 """
-import ast
-import pip.download
 from pip.req import parse_requirements
-import re
 from setuptools import setup, find_packages
 
 
 def get_requirements():
     return [str(r.req) for r in list(parse_requirements(
         'requirements.txt',
-        session=pip.download.PipSession()
     ))]
 
 
 setup(
     name='pir_client',
-    version="0.0.5",
+    version="0.0.1",
     license='MIT',
     author='Department for International Trade',
     description='API client for PIR',
